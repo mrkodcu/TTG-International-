d3155add6c61e395019b03a00ca93c7355959f42
mrkodcu <55021345+mrkodcu@users.noreply.github.com>
2019-09-07 17:43:16 +0300

    Create Question_2_language_PYTHON_method_1.py

---
+++ Question_2_language_PYTHON_method_1.py
@@ -0,0 +1,11 @@
+# TTG International - Mülakat Çözümleri:
+###### 2.SORU PYTHON METHOD 1 ######
+def rotate(array,b):
+    array = array[1:] + array[:b]
+    print(str(array))
+    
+array= [1, 2, 3, 4, 5, 6]
+rotate(array,1)
+
+#Python ile basit bir method.
+####################################
