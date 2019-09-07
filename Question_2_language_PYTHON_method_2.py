a8ba1de7464963d0a9f5d3570e23b6ae1d6da4f9
mrkodcu <55021345+mrkodcu@users.noreply.github.com>
2019-09-07 18:25:10 +0300

    Create Question_2_language_PYTHON_method_2.py

---
+++ Question_2_language_PYTHON_method_2.py
@@ -0,0 +1,16 @@
+# TTG International - Mülakat Çözümleri:
+###### 2.SORU PYTHON METHOD 2 ######
+
+def rotate(array, miktar):
+    uzunluk = len(array)
+    for i in range(miktar):
+        temp = array[0]
+        for i in range(uzunluk - 1):
+            array[i] = array[i + 1]
+        array[uzunluk - 1] = temp
+
+array = [1, 2, 3, 4, 5, 6]
+rotate(array, 1)
+print(array)
+
+#Java methodunun python örneğidir.
